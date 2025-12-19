d5d90d375b3675aa05fb14e33c333e19e15f3c51
kz24031 <k>
2025-12-19 21:38:37 +0200

    final files so all of the functions work in main.py

--- kz24031_main.py
+++ kz24031_main.py
@@ -1,9 +1,10 @@
 # This is a simple calculation program
 # created to demonstrate version control.
-from libadd import *
-from libmult import *
+from kz24031_libadd import *
+from kz24031_libmult import *
 a = 13
 b = 5
 print(plus(a,b))
 print(plusx(a,b,2))
 print(mult(a,b))
+print(multx(a,b,2))
